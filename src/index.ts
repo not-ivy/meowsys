3d882cf1aaea24ae348923b982bc327312ad61fe
ivy <ivy@not-ivy.dev>
2022-12-22 18:35:18 -0800

    add default code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-setInterval(() => {
-  console.log(Game.time)
-}, 1000);
+const init = () => {
+  const spawn = Game.spawns["meow"];
+  const body = [WORK, CARRY, MOVE];
+  const name = "meowh1";
+  spawn.spawnCreep(body, name);
+};
+
+export const loop = () => {};
+
+Object.keys(Game.creeps).length === 0 && init();
+
+module.exports.loop = loop;
